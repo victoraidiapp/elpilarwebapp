523dbdba7f8d1275aba10f20d274fec3a43bd149
Víctor <victorpe76@gmail.com>
2014-02-03 21:48:03 +0100

    alterContent
    Pruebo a añadir esta linea

--- js/funciones.js
+++ js/funciones.js
@@ -325,9 +325,10 @@ $(document).delegate('#navmenu a','tap',function(){
             
     });
 	urldest=$(this).attr('href');
-	$('#container').fadeOut('fast');
+	//$('#container').fadeOut('fast');
 	//alert("Espero a ver si se ha quitado");
 	$.mobile.navigate(urldest);
+	alterContent(urldest);
 	//$( ":mobile-pagecontainer" ).pagecontainer( "change", urldest, { transition: "fade" } );
 return false;
 			})
